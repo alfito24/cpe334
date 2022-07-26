5f59bc50a4d154b3e2ad50876022652dcb2fc41f
michrist <christopher.michael104@gmail.com>
2022-07-27 00:41:37 +0700

    install tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+  <h1 class="text-5xl font-bold text-red-500 m-3" >
+    Hello world!
+  </h1>
+  <a href="#" class="bg-green-600 p3 text-white inline-block m-3 rounded shadow hover:bg-green-700">ini tombol </a>
+  <p class="m-3 bg-yellow-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui soluta maiores ducimus debitis, praesentium maxime in consequatur? Soluta adipisci quibusdam unde molestiae at alias provident assumenda. Minus alias culpa consequatur a quos non possimus fuga, maiores, vitae odio laboriosam dolor, laborum quae omnis? Perferendis illum libero placeat voluptatum, dicta fugit!</p>
+</body>
+</html>
