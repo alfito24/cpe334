e167dc5d3cb35aa961add91a8dc20f71b742ec70
lsatriosim <liefran123@gmail.com>
2022-08-01 17:29:05 +0700

    update User seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,23 +16,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $today = Carbon::now();
+
         DB::table('users')->insert([
+            'user_id' => Str::uuid(),
             'name'=>'Alfito',
             'username'=>'alfito24',
             'email'=>'khansaalfito@gmail.com',
             'password' => bcrypt('user123'),
             'role_id'=>2,
             'alamat'=>'Surabaya',
-            'no_telp'=>'08123456789'
+            'no_telp'=>'08123456789',
+            'created_at' => $today,
+            'updated_at' => $today
         ]);
         DB::table('users')->insert([
+            'user_id' => Str::uuid(),
             'name'=>'Liefran',
             'username'=>'liefran20',
             'email'=>'liefran123@gmail.com',
             'password' => bcrypt('admin123'),
             'role_id'=>1,
             'alamat'=>'Berau',
-            'no_telp'=>'08123456789'
+            'no_telp'=>'08123456789',
+            'created_at' => $today,
+            'updated_at' => $today
         ]);
     }
 }
