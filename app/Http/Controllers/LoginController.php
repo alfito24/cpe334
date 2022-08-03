6645b090a2dd2d8d1dc1fc95e46f1ff0ad3bf73b
lsatriosim <liefran123@gmail.com>
2022-08-04 00:08:48 +0700

    perbaikan routing dan install tailwind

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -18,7 +18,7 @@ class LoginController extends Controller
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
             if(Auth::user()->role_id === 1){
-                return redirect('/dashboard');
+                return redirect('/');
             }
             return redirect('/');
         }
