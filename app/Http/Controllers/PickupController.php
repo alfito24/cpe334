4acca3e2c3c66c622504db526d704935e71005b3
lsatriosim <liefran123@gmail.com>
2022-08-03 20:40:19 +0700

    update store function

--- app/Http/Controllers/PickupController.php
+++ app/Http/Controllers/PickupController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PickupController extends Controller
 {
@@ -12,5 +14,18 @@ class PickupController extends Controller
 
     public function storepickup(Request $request){
         dd($request);
+
+        Transaction::create([
+            'plastic' => $request->jumlah_plastic,
+            'paper' => $request->jumlah_paper,
+            'metals' => $request->jumlah_metals,
+            'glass' => $request->jumlah_glass,
+            'user_id' => Auth::id(),
+            'bpoints' => 0,
+        ])->pickup()->create([
+            'location' => $request->alamatPickup,
+            'pickup_date' => $request->tanggalPickup,
+            'status' => 'Belum Diambil'
+        ]);
     }
 }
--- resources/views/pickup/pickup.blade.php
+++ resources/views/pickup/pickup.blade.php
@@ -138,7 +138,7 @@
                     <h4 class="font-semibold float-right  text-[#66737D]">Rp7.500</h4>
                 </div>
                 <div class="rounded-b-lg border-[#3367AD] border-t-0 border-2 h-11 px-3 py-2.5">
-                    <img src="{{asset('images/points.png')}}" alt="price" class="w-6 inline">
+                    <img src="{{asset('images/pointps.png')}}" alt="price" class="w-6 inline">
                     <h4 class="font-semibold inline pl-2 text-[#66737D]">B Points</h4>
                     <h4 class="font-semibold float-right  text-[#66737D]">75</h4>
                 </div>
