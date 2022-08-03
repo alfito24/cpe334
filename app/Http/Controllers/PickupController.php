c7e3cf3ce579d45a11e164eafc0d9d295797a35a
lsatriosim <liefran123@gmail.com>
2022-08-04 00:10:22 +0700

    store pickup dan transactions

--- app/Http/Controllers/PickupController.php
+++ app/Http/Controllers/PickupController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pickup;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class PickupController extends Controller
 {
@@ -13,7 +15,10 @@ class PickupController extends Controller
     }
 
     public function storepickup(Request $request){
-        dd($request);
+        if($request->ampm == "pm"){
+            $request->hours += 12;
+        }
+        $pickupTime = date('H:i', strtotime($request->hours.":".$request->minutes));
 
         Transaction::create([
             'plastic' => $request->jumlah_plastic,
@@ -21,11 +26,14 @@ class PickupController extends Controller
             'metals' => $request->jumlah_metals,
             'glass' => $request->jumlah_glass,
             'user_id' => Auth::id(),
-            'bpoints' => 0,
+            'bpoints' => $request->bPoints,
         ])->pickup()->create([
             'location' => $request->alamatPickup,
-            'pickup_date' => $request->tanggalPickup,
+            'pickup_date' => date("d/m/y", strtotime($request->tanggalPickup)),
+            'pickup_time' => $pickupTime,
             'status' => 'Belum Diambil'
         ]);
+
+        return redirect()->back()->with('success', 'Sampah akan segera diambil. Mohon ditunggu yah ^-^');
     }
 }
