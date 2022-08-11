e555642d64704ffc0f1eb9bef5a200b750795885
michrist <christopher.michael104@gmail.com>
2022-08-11 09:05:27 +0700

    revisi pick up

--- resources/views/pickup/pickup.blade.php
+++ resources/views/pickup/pickup.blade.php
@@ -68,12 +68,14 @@
             </div>
 
             <div class="flex mt-5">
-                <div class="relative w-full">
-                    <input type="search" id="search" class="block p-2.5 w-full z-20 text-sm text-gray-900 rounded-lg border-[#3367AD] border-2" placeholder="Search your address" required name="alamatPickup">
-                    <button type="submit" class="absolute top-0 right-0 p-2.5 text-sm text-[#66737D] font-medium  rounded-r-lg border border-transparent hover:bg-[#3166AD] hover:text-white">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                        <span class="sr-only">Search</span>
-                    </button>
+                <div class="flex-auto w-[55%] mr-3">
+                    <input type="text" id="address" class="p-2.5 z-20 w-full text-sm text-gray-900 rounded-lg border-[#3367AD] border-2" placeholder="address" required name="alamatPickup">
+                </div>
+                <div class="flex-auto w-[15%] mr-3">
+                    <input type="text" id="no" class=" p-2.5 w-full text-sm text-gray-900 rounded-lg border-[#3367AD] border-2" placeholder="no" required>
+                </div>
+                <div class="flex-auto w-[30%]">
+                    <input type="text" id="city" class="p-2.5 w-full text-sm text-gray-900 rounded-lg border-[#3367AD] border-2" placeholder="city" required>
                 </div>
             </div>
             <div class="mt-5">
